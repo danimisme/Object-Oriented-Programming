c903204504b88878460615188317d6cfd8723662
danimisme <msubhanr53@gmail.com>
2024-06-30 20:10:27 +0700

    membuat autoloading untuk memanggil semua class

--- 12-autoloading/App/init.php
+++ 12-autoloading/App/init.php
@@ -1,7 +1,12 @@
 <?php
 
-require 'Produk/InfoProduk.php';
-require 'Produk/CetakInfoProduk.php';
-require 'Produk/Produk.php';
-require 'Produk/Komik.php';
-require 'Produk/Game.php';
+// require 'Produk/InfoProduk.php';
+// require 'Produk/CetakInfoProduk.php';
+// require 'Produk/Produk.php';
+// require 'Produk/Komik.php';
+// require 'Produk/Game.php';
+
+
+spl_autoload_register(function ($class){
+    require_once __DIR__ .'/Produk/' . $class . '.php';
+});
