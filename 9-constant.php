c74ca04bf00e8b0ac4d8f2b88cd58bd84e5afa26
danimisme <msubhanr53@gmail.com>
2024-06-30 15:47:57 +0700

    magic constant

--- 9-constant.php
+++ 9-constant.php
@@ -15,7 +15,28 @@ class Coba {
 }
 
 echo Coba::NAMA;
+echo "<br>";
 
+echo __LINE__;
+echo "<br>";
+
+echo __FILE__;
+echo "<br>";
+
+function coba() {
+    return __FUNCTION__;
+}
+
+echo coba();
+
+Class CobaKelas {
+    public $kelas = __CLASS__;
+}
+
+echo "<br>";
+
+$obj1 = new CobaKelas();
+echo $obj1->kelas;
 
 
 ?>
