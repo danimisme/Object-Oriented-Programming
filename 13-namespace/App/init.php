39c18bde2fd64032c577c81253b17607110b9ce3
danimisme <msubhanr53@gmail.com>
2024-06-30 20:35:11 +0700

    fix autoload

--- 13-namespace/App/init.php
+++ 13-namespace/App/init.php
@@ -1,15 +1,23 @@
 <?php
 
-require 'Produk/InfoProduk.php';
-require 'Produk/CetakInfoProduk.php';
-require 'Produk/Produk.php';
-require 'Produk/Komik.php';
-require 'Produk/Game.php';
-require 'Produk/User.php';
+// require 'Produk/InfoProduk.php';
+// require 'Produk/CetakInfoProduk.php';
+// require 'Produk/Produk.php';
+// require 'Produk/Komik.php';
+// require 'Produk/Game.php';
+// require 'Produk/User.php';
 
-require 'Service/User.php';
+// require 'Service/User.php';
 
 
-// spl_autoload_register(function ($class){
-//     require_once __DIR__ .'/Produk/' . $class . '.php';
-// });
+spl_autoload_register(function ($class){
+    $class = explode('\\', $class);
+    $class = end($class);
+    require_once __DIR__ .'/Produk/' . $class . '.php';
+});
+
+spl_autoload_register(function ($class){
+    $class = explode('\\', $class);
+    $class = end($class);
+    require_once __DIR__ .'/Service/' . $class . '.php';
+});
