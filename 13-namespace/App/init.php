230e1df53f7e75fbbc2582b3375dbc701c0ce7d1
danimisme <msubhanr53@gmail.com>
2024-06-30 20:27:08 +0700

    create clas s

---
+++ 13-namespace/App/init.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'Produk/InfoProduk.php';
+require 'Produk/CetakInfoProduk.php';
+require 'Produk/Produk.php';
+require 'Produk/Komik.php';
+require 'Produk/Game.php';
+require 'Produk/User.php';
+
+require 'Service/User.php';
+
+
+// spl_autoload_register(function ($class){
+//     require_once __DIR__ .'/Produk/' . $class . '.php';
+// });
