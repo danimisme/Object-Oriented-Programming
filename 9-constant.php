d0087a61f423aeff0c1236033bfaf10473f98ce5
danimisme <msubhanr53@gmail.com>
2024-06-30 15:43:09 +0700

    constanta

---
+++ 9-constant.php
@@ -0,0 +1,21 @@
+<?php 
+
+define('NAMA', 'Muhammad Subhan');
+echo NAMA;
+
+echo "<br>";
+
+const UMUR = 32;
+echo UMUR;
+
+echo "<br>";
+
+class Coba {
+    const NAMA = "Dhani";
+}
+
+echo Coba::NAMA;
+
+
+
+?>
